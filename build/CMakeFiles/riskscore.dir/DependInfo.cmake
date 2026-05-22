b5c21edacd390ad55218758b17ca49f0ec7c8348
lowLevelLucifer <uthkarsh75@gmail.com>
2026-05-22 19:19:08 +0530

    Changes made to the build files libriskscore.so files

---
+++ build/CMakeFiles/riskscore.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bot1/fypOfficalTwo/ZeroTrustEngine/src/profile.c" "CMakeFiles/riskscore.dir/src/profile.c.o" "gcc" "CMakeFiles/riskscore.dir/src/profile.c.o.d"
+  "/home/bot1/fypOfficalTwo/ZeroTrustEngine/src/risk_engine.c" "CMakeFiles/riskscore.dir/src/risk_engine.c.o" "gcc" "CMakeFiles/riskscore.dir/src/risk_engine.c.o.d"
+  "/home/bot1/fypOfficalTwo/ZeroTrustEngine/src/scoring.c" "CMakeFiles/riskscore.dir/src/scoring.c.o" "gcc" "CMakeFiles/riskscore.dir/src/scoring.c.o.d"
+  "/home/bot1/fypOfficalTwo/ZeroTrustEngine/src/security.c" "CMakeFiles/riskscore.dir/src/security.c.o" "gcc" "CMakeFiles/riskscore.dir/src/security.c.o.d"
+  "/home/bot1/fypOfficalTwo/ZeroTrustEngine/src/session.c" "CMakeFiles/riskscore.dir/src/session.c.o" "gcc" "CMakeFiles/riskscore.dir/src/session.c.o.d"
+  "" "libriskscore.so" "gcc" "CMakeFiles/riskscore.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
